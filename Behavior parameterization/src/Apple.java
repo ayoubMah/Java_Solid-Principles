02fb39e797ab08e0c42fc548814b55ed7e0d2de9
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-02-22 00:12:20 +0100

    Behavior parameterization example

---
+++ Behavior parameterization/src/Apple.java
@@ -0,0 +1,26 @@
+public class Apple {
+    private String color ;
+    private int width ;
+
+    public Apple(String color, int width){
+        this.color = color ;
+        this.width = width ;
+
+    }
+
+    public String getColor(){
+        return color ;
+    }
+
+    public int getWidth(){
+        return width ;
+    }
+
+    public void setColor( String color){
+        this.color = color ;
+    }
+
+    public void setWidth( int width){
+        this.width = width ;
+    }
+}
---
+++ Behavior parameterization/src/PrintApple.java
@@ -0,0 +1,5 @@
+import java.util.List;
+
+public interface PrintApple {
+    public String prettyPrintApple(List<Apple> apples, )
+}
